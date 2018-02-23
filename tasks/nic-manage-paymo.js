9610c3a02d514657a924cd0fb58869a57f5be99b
Greg Nemes <mail@gregnemes.com>
2018-02-23 12:40:10 -0500

    changed manage paymo assignment times

--- tasks/nic-manage-paymo.js
+++ tasks/nic-manage-paymo.js
@@ -20,8 +20,8 @@ module.exports = {
     active: true,
     schedule: {
         second: "0",
-        minute: "30", // 0 - 59
-        hour: "14",// 0 - 23
+        minute: "45", // 0 - 59
+        hour: "12",// 0 - 23
         dayOfMonth: "*", // 1 - 31
         month: "*",// 1 - 31
         dayOfWeek: "1-5" // [0...7], where both 0 and 7 are sunday.
